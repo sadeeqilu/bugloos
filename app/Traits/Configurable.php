facaa48930235e6fe9750b9322ab5183ad69096d
sadeeqilu <ilusadiq@gmail.com>
2022-12-02 11:14:35 +0100

    add configurable trait for setting up configurations

---
+++ app/Traits/Configurable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Traits;
+
+use ReflectionClass;
+
+/**
+ * Class Configurable
+ */
+trait Configurable
+{
+    /**
+     * @param array $config
+     */
+    public function loadConfig(array $config = [])
+    {
+        $attributes = $this->attributes();
+
+        foreach ($config as $key => $value) {
+            if (!in_array($key, $attributes)) {
+                continue;
+            }
+            $this->{$key} = $value;
+        }
+    }
+
+    /**
+     * @return array
+     */
+    protected function attributes()
+    {
+        $class = new ReflectionClass($this);
+        $names = [];
+        foreach ($class->getProperties() as $property) {
+            if (!$property->isStatic()) {
+                $names[] = $property->getName();
+            }
+        }
+
+        return $names;
+    }
+}
